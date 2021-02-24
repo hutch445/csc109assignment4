4712652782652fadbcbde48fb38e4f3a6a5b683c
hutch445 <65507983+hutch445@users.noreply.github.com>
2021-02-24 18:58:17 -0500

    Added Chinese Hello World output

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 		System.out.println("Hola World!");
 		System.out.println("Privyet mir!");
 		System.out.println("kak vas zovut?");
-
+		System.out.println("Ni hao, shijie!");
 	}
 
 }
