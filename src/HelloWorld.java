bee23ec5cefd0be886c9518d92bc5e98f2832c54
hutch445 <65507983+hutch445@users.noreply.github.com>
2021-02-24 18:54:19 -0500

    Added name question

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hola World!");
 		System.out.println("Privyet mir!");
+		System.out.println("kak vas zovut?");
 
 	}
 
